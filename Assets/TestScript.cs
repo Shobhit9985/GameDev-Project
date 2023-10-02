28e9d35e2dba8e5d36c002cfd479d3471bfd6a25
QTMushy <shubhammalhotra311@gmail.com>
2023-10-02 18:41:06 -0400

    Test Commit

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,13 +7,14 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello");
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("hello world");
+
     }
 }
