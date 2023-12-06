d759678e439aa23c54f13ebefb89568f59a2406f
Shubham Malhotra <malhot91@uwindsor.ca>
2023-12-06 13:10:59 -0500

    Crouch camera

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    private bool crouch = false;
+    public GameObject lookAt;
 
     private Rigidbody rb; // Use Rigidbody for 3D games
 
@@ -18,6 +20,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        Crouching();
     }
 
     void MovePlayer()
@@ -27,5 +30,23 @@ public class Movement : MonoBehaviour
 
         Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed;
         rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+    }
+
+    private void Crouching()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            if (crouch == false)
+            {
+                lookAt.transform.position = new Vector3(lookAt.transform.position.x, lookAt.transform.position.y / 2f, lookAt.transform.position.z);
+                crouch = true;
+
+            }
+            else
+            {
+                lookAt.transform.position = new Vector3(lookAt.transform.position.x, lookAt.transform.position.y * 2f, lookAt.transform.position.z);
+                crouch = false;
+            }
+        }
     }
 }
