0c0e84adaf3a0b0e71d23ee6a38de02cddf45984
Shubham Malhotra <malhot91@uwindsor.ca>
2023-12-09 19:22:02 -0500

    Shop Added with dummy coins

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float playerHealth = 100;
+    float playerCollectedCoins = 0;
+    public static GameManager instance;
+    private GameState gameState;
+
+    private void Start()
+    {
+        gameState = GameState.playing;
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   public enum GameState
+    {
+        paused,
+        playing,
+        buyMenuPaused
+    }
+
+    public void StateUpdate(GameState state)
+    {
+        this.gameState = state;
+    }
+
+    public GameState GetGameState()
+    {
+        return gameState;
+    }
+
+    public void PlayerDamage(float damage)
+    {
+        playerHealth -= damage;
+    }
+}
